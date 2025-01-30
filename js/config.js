fd705ed25b0a199c1e520a84d7f707d77cce4730
South Hills Code <rfitzgerald@c-vusd.org>
2025-01-30 14:23:53 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_UmvU8iSB2', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '75484682fianq7td98ne42sdh9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_uhh4q9wlb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3mf91top54mqehq9ooqjpop77k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
